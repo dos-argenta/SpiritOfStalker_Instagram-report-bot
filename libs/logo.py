b9b4708c8edcfdca557763e3332917a73fd625a5
DOS~ARGENTA <94066739+dos-argenta@users.noreply.github.com>
2022-03-23 15:31:36 +0100

    Add files via upload

---
+++ libs/logo.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+#!/usr/bin/env python3
+from colorama import Fore, Back, Style
+from random import choice
+
+logo =  """
+
+┏━━━┓╋╋╋╋╋╋┏┓╋┏━━━┓┏━┓┏━━━┓┏┓╋╋╋┏┓┏┓
+┃┏━┓┃╋╋╋╋╋┏┛┗┓┃┏━┓┃┃┏┛┃┏━┓┣┛┗┓╋╋┃┃┃┃
+┃┗━━┳━━┳┳━╋┓┏┛┃┃╋┃┣┛┗┓┃┗━━╋┓┏╋━━┫┃┃┃┏┳━━┳━┓
+┗━━┓┃┏┓┣┫┏╋┫┃╋┃┃╋┃┣┓┏┛┗━━┓┃┃┃┃┏┓┃┃┃┗┛┫┃━┫┏┛
+┃┗━┛┃┗┛┃┃┃┃┃┗┓┃┗━┛┃┃┃╋┃┗━┛┃┃┗┫┏┓┃┗┫┏┓┫┃━┫┃
+┗━━━┫┏━┻┻┛┗┻━┛┗━━━┛┗┛╋┗━━━┛┗━┻┛┗┻━┻┛┗┻━━┻┛
+╋╋╋╋┃┃
+╋╋╋╋┗┛
+┏━━┓╋╋╋╋╋┏┓╋╋╋┏━━━┓╋╋╋╋╋╋╋╋╋╋┏┓
+┗┫┣┛╋╋╋╋┏┛┗┓╋╋┃┏━┓┃╋╋╋╋╋╋╋╋╋┏┛┗┓
+╋┃┃┏━┓┏━┻┓┏╋━━┫┗━┛┣━━┳━━┳━━┳┻┓┏┛
+╋┃┃┃┏┓┫━━┫┃┃┏┓┃┏┓┏┫┃━┫┏┓┃┏┓┃┏┫┃
+┏┫┣┫┃┃┣━━┃┗┫┏┓┃┃┃┗┫┃━┫┗┛┃┗┛┃┃┃┗┓
+┗━━┻┛┗┻━━┻━┻┛┗┻┛┗━┻━━┫┏━┻━━┻┛┗━┛
+╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┃┃
+╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┗┛
+                                                                   
+                                                                   """
+
+
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)                                                  
+    print(Fore.RED + "                                                      Youtube" + Fore.CYAN + " - DOS-ARGENTA")
+    print(Fore.RED + "                                                      BY     " + Fore.CYAN + " - DOS-ARGENTA x SpiritOfStalker ")
+    print(Fore.RED + "                                                      Discord"+ Fore.CYAN + " - https://discord.gg/gxtrRGGpWZ ")
+    print(Fore.GREEN + "      GitHub: https://github.com/dos-argenta "+ Style.RESET_ALL + Style.BRIGHT)
